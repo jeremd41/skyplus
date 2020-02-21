1bffa853bf4eac54833785b92885c89330f85df7
Jérémy DIARD <44467357+jeremd41@users.noreply.github.com>
2020-02-21 10:45:18 +0100

    references ok

--- src/components/cardReferences.js
+++ src/components/cardReferences.js
@@ -1,32 +1,38 @@
-import React from  "react"
+import React, { Component } from "react"
 import styled from "styled-components"
 
 const Wrapper = styled.div`
-width :30%
-border-radius : 10px ;
-box-shadow: 0px 2px 10px rgba(0,0,0,0.15);
+  width: 30%;
+  border-radius: 10px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.15);
+  padding: 10px;
 
-.reference-image{
-width : 310px ;
-height : auto ;
-display :block ;
-margin : 0 auto ;
-}
+  .reference-image {
+    width: 90%;
+    height: auto;
+    display: block;
+    margin: 0 auto;
+  }
 
-reference-title{
-text-align : center ;
-margin : 25px 0 ;
-}
+  .reference-title {
+    text-align: center;
+    margin: 50px 0;
+  }
 `
-class cardReferences extends Component{
- render(){
-  return(
-<Wrapper>
-<img className="reference-image" src={this.props.imageRef} alt="reference"/>
-<h4 className="reference-title">{this.props.nameRef}</h4>
-</Wrapper>
-)
-}
+
+class cardReferences extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <img
+          className="reference-image"
+          src={this.props.imageRef}
+          alt="reference"
+        />
+        <h4 className="reference-title">{this.props.nameRef}</h4>
+      </Wrapper>
+    )
+  }
 }
 
 export default cardReferences
--- src/pages/references.js
+++ src/pages/references.js
@@ -1,17 +1,82 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import TopPage from "../components/topPage"
 
 import Cover from "../images/header-skyplus.jpg"
+import CardReferences from "../components/cardReferences"
 
-const references = () => (
-  <Layout>
-    <SEO title="Home" />
-    <TopPage imageTop={Cover} titleTop="Nos références" />
-  </Layout>
-)
+import ref1 from "../images/reference/1.png"
+
+const Wrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+
+  .references-title {
+    text-align: center;
+  }
+  .references-middle-wrap {
+    width: 100%;
+    margin: 50px auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+`
+
+class references extends Component {
+  state = {
+    middle: [
+      {
+        image: ref1,
+        title: "test",
+      },
+      {
+        image: ref1,
+        title: "test",
+      },
+      {
+        image: ref1,
+        title: "test",
+      },
+      {
+        image: ref1,
+        title: "test",
+      },
+      {
+        image: ref1,
+        title: "test",
+      },
+      {
+        image: ref1,
+        title: "test",
+      },
+    ],
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <TopPage imageTop={Cover} titleTop="Nos références" />
+        <Wrapper>
+          <h1 className="references-title">Nos références</h1>
+          <div className="references-middle-wrap">
+            {this.state.middle.map((item, index) => {
+              return (
+                <CardReferences
+                  key={index}
+                  imageRef={item.image}
+                  nameRef={item.title}
+                />
+              )
+            })}
+          </div>
+        </Wrapper>
+      </Layout>
+    )
+  }
+}
 
 export default references
